1ce0db3cd047452317e7b343a633990a1dccca24
Mikael Engström <mikael.pj.engstrom@gmail.com>
2019-01-17 17:43:21 +0100

    Fix bad validation for Swish payments
    Update regex to use the same rules required by the actual payment portal. Earlier it were validating Norwegian numbers which does not really make sense for Swish

--- includes/class-wc-gateway-payex-swish.php
+++ includes/class-wc-gateway-payex-swish.php
@@ -197,9 +197,9 @@ class WC_Gateway_Payex_Psp_Swish extends WC_Gateway_Payex_Cc
 		}
 
 		$matches = array();
-		preg_match( '/^(\+47)(?:4[015-8]|5[89]|87|9\d)\d{6}$/u', $billing_phone, $matches );
+		preg_match( '/^\+46[0-9]{6,13}$/u', $billing_phone, $matches );
 		if ( ! isset( $matches[0] ) || $matches[0] !== $billing_phone ) {
-			wc_add_notice( __( 'Input your number like this +47xxxxxxxxx', 'woocommerce-gateway-payex-psp' ), 'error' );
+			wc_add_notice( __( 'Input your number like this +46xxxxxxxxx', 'woocommerce-gateway-payex-psp' ), 'error' );
 			return FALSE;
 		}
 
