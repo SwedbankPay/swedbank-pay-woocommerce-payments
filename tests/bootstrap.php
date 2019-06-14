1c3106034420dc152c3c9b39a7e712cf13cc469b
aait <info@aait.se>
2019-06-14 10:46:21 +0600

    Fixed tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,7 +21,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/woocommerce-payex-psp.php';
+	require dirname( dirname( __FILE__ ) ) . '/payex-woocommerce-payments.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
