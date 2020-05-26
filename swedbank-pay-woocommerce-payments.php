35100b85689a37caad3c391a54051a6ed97f3bb3
aait <info@aait.se>
2020-05-26 17:28:56 +0600

    Show hint if composer dependencies weren't installed

--- swedbank-pay-woocommerce-payments.php
+++ swedbank-pay-woocommerce-payments.php
@@ -42,6 +42,11 @@ class WC_Swedbank_Pay extends WC_Swedbank_Plugin {
 	 * Install
 	 */
 	public function install() {
+		// Check dependencies
+		if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+			die( 'This plugin can\'t be activated. Please run `composer install` to install dependencies.' );
+		}
+
 		parent::install();
 	}
 
