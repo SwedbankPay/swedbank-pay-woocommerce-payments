06df140e0d618ca75f8d09cbfa4a295001b5fa7a
aait <info@aait.se>
2021-09-07 15:27:22 +0600

    Catch all exceptions

--- includes/class-wc-gateway-swedbank-pay-cc.php
+++ includes/class-wc-gateway-swedbank-pay-cc.php
@@ -907,7 +907,7 @@ class WC_Gateway_Swedbank_Pay_Cc extends WC_Payment_Gateway {
 			wp_send_json_success( array(
 				'state' => 'unknown',
 			) );
-		} catch ( \SwedbankPay\Core\Exception $exception ) {
+		} catch ( \Exception $exception ) {
 			wp_send_json_success( array(
 				'state' => 'failed',
 				'message' => $exception->getMessage()
